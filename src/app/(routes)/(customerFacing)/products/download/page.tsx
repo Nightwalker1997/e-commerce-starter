f56dc6970815bf60f58ca57cee1bd80c177f9036
gholamreza falahzade <gholamreza.fallah2208@gmail.com>
2024-05-18 22:20:03 +0330

    /products/download/page.tsx' was not a module

---
+++ src/app/(routes)/(customerFacing)/products/download/page.tsx
@@ -0,0 +1,9 @@
+const DownloadPage = () => {
+    return(
+        <div className="flex items-center justify-center text-3xl font-bold text-red-base">
+            Wrong Address :(
+        </div>
+    )
+}
+
+export default DownloadPage;
