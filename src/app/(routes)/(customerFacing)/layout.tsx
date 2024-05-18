e25992414598845ea63918ebcc390187912a3c5d
gholamreza falahzade <gholamreza.fallah2208@gmail.com>
2024-05-18 22:14:16 +0330

    fixed some problem in customerfacing layout dynamic

--- src/app/(routes)/(customerFacing)/layout.tsx
+++ src/app/(routes)/(customerFacing)/layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/navbar";
 import NavLink from "@/components/navbar/navLink";
 // don't save data in cache and be always dynamic so data can be up to date
-export const dynamic = "force-hynamic"
 
 const AdminLayout = ({
     children
