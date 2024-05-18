75d4ebb8c562bd4df0a28a2dd800bf5284daf191
gholamreza falahzade <gholamreza.fallah2208@gmail.com>
2024-05-18 22:38:45 +0330

    wtf with this f**king bugs َییبیشس

--- src/app/(routes)/admin/products/[productId]/download/route.ts
+++ src/app/(routes)/admin/products/[productId]/download/route.ts
@@ -4,24 +4,29 @@ import { NextRequest, NextResponse } from "next/server";
 import fs from "fs/promises";
 
 export async function GET(
-  req: NextRequest,
-  { params: { id } }: { params: { id: string } }
-) {
-  const product = await prisma.product.findUnique({
-    where: { id },
-    select: { filePath: true, name: true },
-  })
+//   req: NextRequest,
+    req: Request,
+    { 
+    // params: { id } }: { params: { id: string } 
+}) {
+    // const product = await prisma.product.findUnique({
+    //     where: { id },
+    //     select: { filePath: true, name: true },
+    // })
 
-  if (product == null) return notFound()
+    // if (product == null) return notFound()
 
-  const { size } = await fs.stat(product.filePath)
-  const file = await fs.readFile(product.filePath)
-  const extension = product.filePath.split(".").pop()
+    // const { size } = await fs.stat(product.filePath)
+    // const file = await fs.readFile(product.filePath)
+    // const extension = product.filePath.split(".").pop()
 
-  return new NextResponse(file, {
-    headers: {
-      "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
-      "Content-Length": size.toString(),
-    },
-  })
+    // return new NextResponse(file, {
+    //     headers: {
+    //         "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
+    //         "Content-Length": size.toString(),
+    //     },
+    // })
+
+    return NextResponse.redirect(new URL("/products/download/expired", req.url));
+
 }
