500db68efe01d6bd686c86b4da1a0705e836541a
gholamreza falahzade <gholamreza.fallah2208@gmail.com>
2024-05-18 22:26:13 +0330

    fiex some problem in type in /src/email/PurchaseReceipt.tsx

--- src/app/(routes)/(customerFacing)/layout.tsx
+++ src/app/(routes)/(customerFacing)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/navbar";
 import NavLink from "@/components/navbar/navLink";
 // don't save data in cache and be always dynamic so data can be up to date
+// export const dynamic = "force-hynamic"
 
 const AdminLayout = ({
     children
